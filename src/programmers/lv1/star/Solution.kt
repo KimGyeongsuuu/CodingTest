2e5c3ef9d668ef00884885b07e5ac965fef07836
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-14 22:59:01 +0900

    직사각형 별찍기(2023.07.14)

---
+++ src/programmers/lv1/star/Solution.kt
@@ -0,0 +1,11 @@
+package programmers.lv1.star
+
+fun main() {
+    val (a, b) = readln().split(' ').map(String::toInt)
+    for (i in 0 until b) {
+        for (j in 0 until a) {
+            print("*")
+        }
+        println()
+    }
+}
