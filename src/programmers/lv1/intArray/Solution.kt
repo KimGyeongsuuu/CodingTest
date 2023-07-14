a89a4ed9988b1dd48440e7a2ad8e661c30fcb281
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-14 22:16:49 +0900

    나누어 떨어지는 숫자 배열 (2023.07.14)

---
+++ src/programmers/lv1/intArray/Solution.kt
@@ -0,0 +1,22 @@
+package programmers.lv1.intArray
+
+class Solution {
+    fun solution(arr: IntArray, divisor: Int): IntArray {
+        var answer = intArrayOf()
+        for (i in arr) {
+            if (i % divisor == 0) {
+                answer = answer.plus(i)
+            }
+        }
+        if (answer.isEmpty()) {
+            answer = answer.plus(-1)
+        }
+        answer.sort()
+        return answer
+    }
+}
+fun main() {
+    val s = Solution()
+    val sol = s.solution(intArrayOf(5,9,7,10),5)
+    println(sol.contentToString())
+}
