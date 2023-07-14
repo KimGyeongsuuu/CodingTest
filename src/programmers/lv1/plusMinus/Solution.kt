5719ee65d7080f7f8756cd4386a8c8e169151528
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-14 22:39:23 +0900

    음양 더하기(2023.07.14)

---
+++ src/programmers/lv1/plusMinus/Solution.kt
@@ -0,0 +1,17 @@
+package programmers.lv1.plusMinus
+
+class Solution {
+    fun solution(absolutes: IntArray, signs: BooleanArray): Int {
+        var answer: Int = 0
+        for (i in absolutes.indices) {
+            val value = if (signs[i]) absolutes[i] else -absolutes[i]
+            answer+=value
+        }
+        return answer
+    }
+}
+fun main() {
+    val s = Solution()
+    val sol = s.solution(intArrayOf(4,7,12), booleanArrayOf(true,false,true))
+    println(sol)
+}
