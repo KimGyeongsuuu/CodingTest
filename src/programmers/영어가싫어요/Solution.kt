3dacc180417c3f6c262f7e71895dcf9dc0cbfd5d
KimGyeongsuuu <sk62628504@gmail.com>
2023-11-26 01:08:09 +0900

    영어가 싫어요 (2023.11.26)

---
+++ src/programmers/영어가싫어요/Solution.kt
@@ -0,0 +1,26 @@
+package programmers.영어가싫어요
+class Solution {
+    fun solution(numbers: String): Long {
+        val map = mapOf(
+            "zero" to "0",
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9"
+        )
+        var result = numbers
+        map.forEach {
+            result = result.replace(it.key, it.value)
+        }
+        return result.toLong()
+    }
+}
+fun main() {
+    val s = Solution()
+    print(s.solution("zeroonefourzerosixseven"))
+}
