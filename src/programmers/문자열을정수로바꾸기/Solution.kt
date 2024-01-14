ece5af0a010f57c85314cd24eef315ab6233ab99
KimGyeongsuuu <sk62628504@gmail.com>
2024-01-14 15:30:44 +0900

    문자열을 정수로 바꾸기 (2024.1.14)

---
+++ src/programmers/문자열을정수로바꾸기/Solution.kt
@@ -0,0 +1,10 @@
+package programmers.문자열을정수로바꾸기
+
+class Solution {
+    fun solution(s: String): Int = s.toInt()
+}
+fun main() {
+    val s = Solution()
+    val sol = s.solution("-1234")
+    println(sol)
+}
