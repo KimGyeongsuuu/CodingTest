d1b5e4832bcc490fc2d9c33b7275ea2590f6b74d
KimGyeongsuuu <sk62628504@gmail.com>
2024-02-02 23:00:00 +0900

    시저 암호 (2024.2.2)

--- src/programmers/caesar/Solution.kt
+++ src/programmers/caesar/Solution.kt
@@ -3,10 +3,11 @@ package programmers.caesar
 class Solution {
     fun solution(s: String, n: Int): String {
         return s.map {
-            if (it.toInt() == 32) {
+            if (it.toInt() == 32) { // 빈칸 일때
                 it.toChar()
-            }
-            else if (it.toInt() + n < 123) {
+            } else if (it.toInt() in 97..122 && it.toInt() + n in 97..122) { // 소문자 일때
+                (it.toInt() + n).toChar()
+            } else if (it.toInt() in 65..90 && it.toInt() + n in 65..90) { // 대문재 일때
                 (it.toInt() + n).toChar()
             } else {
                 (it.toInt()-26 + n).toChar()
@@ -16,6 +17,6 @@ class Solution {
 }
 fun main(){
     val s = Solution()
-    val sol = s.solution("a B z",4)
+    val sol = s.solution("ABCDEFGHIJKLMNOPQRSTUVWXYZ",25)
     println(sol)
 }
