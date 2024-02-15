0431f423771c22168959c62d93a53835f888465e
KimGyeongsuuu <sk62628504@gmail.com>
2024-02-16 01:09:07 +0900

    덧칠하기 (2024.2.16)

--- src/programmers/덧칠하기/Solution.kt
+++ src/programmers/덧칠하기/Solution.kt
@@ -2,22 +2,19 @@ package programmers.덧칠하기
 
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
-        var answer: Int = 1
-        var value = 0
-        for (i in 1 until section.size) {
-            value+=section[section.size-i]-section[section.size-i-1]
-        }
-        if (value >= m) {
-            answer += value / m
-        }
-        if (answer > section.size) {
-            answer = section.size
+        var answer: Int = 0
+        var k = 0
+        for (i in section) {
+            if (i > k) {
+                k += m - 1
+                answer++
+            }
         }
         return answer
     }
 }
 fun main() {
     val s = Solution()
-    val sol = s.solution(5,5, intArrayOf(3))
+    val sol = s.solution(4,1, intArrayOf(1,2,3,4))
     println(sol)
 }
